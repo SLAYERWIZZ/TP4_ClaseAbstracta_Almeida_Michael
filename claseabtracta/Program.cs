1e3f88495ac4c99c2348dd013bbc57de2b8c6970
UTI <UTI@LAB3PC23-UIO>
2024-05-15 07:58:06 -0500

    Agregar archivos de proyecto.

---
+++ claseabtracta/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+abstract class Animal
+{
+    public abstract void animalSound();
+}
+
+class Cerdo : Animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("El cerdo dice: oink oink");
+    }
+}
+
+class Perro : Animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("El perro dice: guau guau");
+    }
+}
+
+class Gato : Animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("El gato dice: miau miau");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Cerdo miCerdo = new Cerdo();
+        miCerdo.animalSound();
+
+        Perro miPerro = new Perro();
+        miPerro.animalSound();
+
+        Gato miGato = new Gato();
+        miGato.animalSound();
+    }
+}
